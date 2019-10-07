b230a19ee02863fc967590f0078a2471e30cab67
Bobby Jenrow <bobbyjenrow@cobblehilldigital.com>
2019-10-07 10:43:53 -0400

    Initial Commit

---
+++ bin/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = env => ({
+    entry: path.resolve(__dirname, '../src/index.js'),
+    mode: env.NODE_ENV,
+    output: {
+        filename: 'ch.js',
+        path: path.resolve(__dirname, 'assets'),
+    },
+    module: {
+        rules: [{
+            test: /\.s?css$/,
+            use: [
+                { loader: MiniCssExtractPlugin.loader, options: {}},
+                'css-loader', 
+                'postcss-loader', 
+                'sass-loader'
+            ]
+        },
+        {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }
+    ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // all options are optional
+            filename: 'ch.css',
+            chunkFilename: '[id].css',
+            ignoreOrder: false, // Enable to remove warnings about conflicting order
+        }),
+    ]
+});
