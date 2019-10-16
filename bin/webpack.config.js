a14592f226192663fa20e0c3d31d4356521d926b
Bobby Jenrow <bobbyjenrow@cobblehilldigital.com>
2019-10-16 10:40:26 -0400

    Added postcss dependencies to webpack config

--- bin/webpack.config.js
+++ bin/webpack.config.js
@@ -6,29 +6,41 @@ module.exports = env => ({
     mode: env.NODE_ENV,
     output: {
         filename: 'ch.js',
-        path: path.resolve(__dirname, 'assets'),
+        path: path.resolve(__dirname, '../assets'),
     },
     module: {
         rules: [{
-            test: /\.s?css$/,
-            use: [
-                { loader: MiniCssExtractPlugin.loader, options: {}},
-                'css-loader', 
-                'postcss-loader', 
-                'sass-loader'
-            ]
-        },
-        {
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
+                test: /\.s?css$/,
+                use: [{
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {}
+                    },
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: (loader) => [
+                                require('postcss-import')(),
+                                require('postcss-preset-env')(),
+                                // require('postcss-liquid-variables')(),
+                                // require('cssnano')()
+                            ]
+                        }
+                    },
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
                 }
             }
-        }
-    ]
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
